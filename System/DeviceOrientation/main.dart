25dabac66beac6c1f8ef92813d1d488ff75198df
Varun Poojary <74899684+VarunPoojary@users.noreply.github.com>
2022-12-19 19:35:56 +0530

    Create main.dart

---
+++ System/DeviceOrientation/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+void main() {
+  
+  
+  
+  
+  
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  
+  
+  
+  
+  runApp(MyApp());
+}
