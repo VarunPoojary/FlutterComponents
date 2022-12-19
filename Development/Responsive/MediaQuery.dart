52c54f8e3c8a12aa6d496c6261fa5ef2a209b5e8
Varun Poojary <74899684+VarunPoojary@users.noreply.github.com>
2022-12-19 21:51:42 +0530

    Update MediaQuery.dart

--- Development/Responsive/MediaQuery.dart
+++ Development/Responsive/MediaQuery.dart
@@ -1,1 +1,14 @@
+//To declare MediaQuery for height
+var h = MediaQuery.of(context).size.height;
+//To declare MediaQuery for width
+var h = MediaQuery.of(context).size.width;
 
+
+
+
+//Usage Example
+SizedBox(
+  height: h*0.3,
+  width: w*0.6,
+  child: Image.asset('assets/images/Elipse.png'),
+        ),
