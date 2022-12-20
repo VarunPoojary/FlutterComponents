8d49b3949d49303aee11212b963131ddc814e0f3
Varun Poojary <74899684+VarunPoojary@users.noreply.github.com>
2022-12-20 10:47:07 +0530

    Create CheckDeviceOrientation.dart

---
+++ System/CheckDeviceOrientation/CheckDeviceOrientation.dart
@@ -0,0 +1,14 @@
+
+// To Get Device orientation
+MediaQuery.of(context).orientation
+  
+// To check if Device is in Portrait mode
+final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+
+
+// To check if Device is in Portrait mode
+final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+
+
+
+// It isPortrait and isLandscape hold a boolean value
