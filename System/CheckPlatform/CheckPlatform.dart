377b10dc75e6272f9be14dc8ade1d0a74591870a
Varun Poojary <74899684+VarunPoojary@users.noreply.github.com>
2022-12-20 22:02:25 +0530

    Update CheckPlatform.dart

--- System/CheckPlatform/CheckPlatform.dart
+++ System/CheckPlatform/CheckPlatform.dart
@@ -1,1 +1,9 @@
+// Import Package
+import 'dart:io';
 
+//Usage
+    var isIos = Platform.isIOS;
+    var isAndroid = Platform.isAndroid;
+    var isLinux = Platform.isLinux;
+    var isMacOs = Platform.isMacOS;
+    var isWindows = Platform.isWindows;
